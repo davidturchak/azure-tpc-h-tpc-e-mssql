865d88ea0e8f222df93388db3c228261517fd64d
David Turchak <david.turchak@gmail.com>
2024-04-01 15:45:23 +0300

    init commit

--- env-bring-up.sh
+++ env-bring-up.sh
@@ -13,6 +13,27 @@ VMSIZE='Standard_L48s_v3'
 IMAGE2USE='microsoftsqlserver:sql2022-ws2022:sqldev-gen2:latest'
 VMADMINUSER='flexadm'
 VMADMINPASS='loPZ7apx0k4ASXucg_'
+#Backup Disk params
+snapshot_name="Azure-TPCH-TPCE-backup"
+resource_group_snapshot="pathfinder-azure-rg"
+new_disk_name="BackupDisk"
+new_disk_sku="StandardSSD_LRS"
+#disk_size_gb=100
+
+# Function to create a managed disk from a snapshot and attach it to the VM
+create_disk_from_snapshot() {
+  echo "Creating BackupDisk..."
+  # Get the snapshot ID
+  local snapshot_id=$(az snapshot show --name $snapshot_name --resource-group $resource_group_snapshot --query [id] -o tsv)
+
+  # Create a managed disk from the snapshot
+  az disk create \
+    --resource-group $RGNAME\
+    --name $new_disk_name \
+    --sku $new_disk_sku \
+    --source $snapshot_id
+  #  --size-gb $disk_size_gb \
+}
 
 # Function to create NICs
 create_nics() {
@@ -46,6 +67,7 @@ create_vm() {
     --nics ${VMNAME}-mgmt ${VMNAME}-data \
     --location $LOCATION \
     --zone $ZONE \
+    --attach-data-disks $new_disk_name \
     --output table
 }
 
@@ -64,6 +86,8 @@ delete_vm() {
 
 # Main script
 if [[ "$1" == "--create" ]]; then
+
+  create_disk_from_snapshot || { echo "Error creating a BackupDisk. Exiting."; exit 1; }
   create_nics || { echo "Error creating NICs. Exiting."; exit 1; }
   create_vm || { echo "Error creating VM. Exiting."; exit 1; }
   echo "Resources created successfully."
