ca7cb147d74af74d8ed8a47e03fe2afca375b195
David Turchak <david.turchak@gmail.com>
2024-04-03 17:21:14 +0300

    Modifyed disk params

--- env-bring-up.sh
+++ env-bring-up.sh
@@ -17,7 +17,7 @@ VMADMINPASS='loPZ7apx0k4ASXucg_'
 snapshot_name="azure_tpc_e_tcpc_h_tools"
 resource_group_snapshot="pathfinder-azure-rg"
 new_disk_name="BackupDisk"
-new_disk_sku="Premium_LRS"
+new_disk_sku="PremiumV2_LRS"
 #disk_size_gb=100
 
 # Function to create a managed disk from a snapshot and attach it to the VM
@@ -32,12 +32,11 @@ create_disk_from_snapshot() {
     --name $new_disk_name \
     --sku $new_disk_sku \
     --source $snapshot_id \
-    --performance-plus \
-    --output table
-    #--zone $ZONE \
-    #--disk-iops-read-write 10000 \
-    #--disk-mbps-read-write 1200 \
-    #--max-shares 1 \
+    --output table \
+    --zone $ZONE \
+    --disk-iops-read-write 5000 \
+    --disk-mbps-read-write 1000 \
+    --max-shares 1
     #  --size-gb $disk_size_gb \
 }
 
